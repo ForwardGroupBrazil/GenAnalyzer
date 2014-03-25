455eb02e24ea8d1a9b67f33b7dc24feb2a415b0f
Diego Matos Figueiredo <dmf@cern.ch>
2014-03-25 20:27:49 +0100

    Update Test

--- AnalyzerMC/test/analyzeTest.py
+++ AnalyzerMC/test/analyzeTest.py
@@ -7,8 +7,9 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("PoolSource",
-        #fileNames = cms.untracked.vstring("file:/storage1/dmf/TestSamples/DyToMuMuPU2010/DyToMuMu.root")
-         fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/d/dmf/private/PrivateMCProduction/Pomwig/CMSSW_6_2_0_patch1/src/POMWIG_SingleDiffractivePlusWEnu_7TeV_cff_py_GEN_SIM.root")
+        #fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/d/dmf/private/PrivateMCProduction/Pomwig/CMSSW_6_2_0_patch1/src/POMWIG_SingleDiffractivePlusWEnu_7TeV_cff_py_GEN_SIM.root")
+        #fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/d/dmf/private/PrivateMCProduction/Pomwig/CMSSW_6_2_0_patch1/src/POMWIG_SingleDiffractivePlusWmunu_7TeV_cff_py_GEN_SIM.root")
+       fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/d/dmf/private/PrivateMCProduction/Pomwig/CMSSW_6_2_0_patch1/src/POMWIG_SingleDiffractivePlusZee_7TeV_cff_py_GEN_SIM.root")
 )
 
 ###process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -22,17 +23,21 @@ process.source = cms.Source("PoolSource",
 ## electron: 11
 ## positron: -11
 ## neutrino electron: 12
+## antineutrino electron: -12
 ## muon: 13
 ## antimuon: -13
 ## neutrino muon: 14
+## antineutrino muon: -14
 ##
+## POMWIG W->E+Nu, PDGId(24)->PDGId(11)+PDGId(12)
+## POMWIG W->Mu+Nu, PDGId(24)->PDGId(14)+PDGId(13)
 
-process.SDDY = cms.EDAnalyzer("SDWAnalyzer",
+process.SDW = cms.EDAnalyzer("SDWAnalyzer",
 	GenParticleTag = cms.InputTag("genParticles"),
-	Particle1Id = cms.int32(13),
-	Particle2Id = cms.int32(14),
+	Particle1Id = cms.int32(11), # fabs(Particle1Id)
+	Particle2Id = cms.int32(11), # fabs(Particle2Id)
         EBeam = cms.double(3500),
-        cmsAccept = cms.untracked.bool(True),
+        cmsAccept = cms.untracked.bool(False),
 	debug = cms.untracked.bool(True)
 )
 
@@ -51,6 +56,6 @@ process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
                                    status = cms.untracked.vint32( 3,2,1 )
                                    )
 
-#process.analysis = cms.Path(process.genParticles*process.SDDY*process.printTree)
-#process.analysis = cms.Path(process.genParticles*process.SDDY)
-process.analysis = cms.Path(process.SDDY)
+#process.analysis = cms.Path(process.genParticles*process.SDW*process.printTree)
+#process.analysis = cms.Path(process.genParticles*process.SDW)
+process.analysis = cms.Path(process.SDW)
